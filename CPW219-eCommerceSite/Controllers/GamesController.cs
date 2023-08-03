b77253f8263ffce311f6721e40173438ca3f4ec9
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-08-03 14:45:34 -0700

    Create game fully functional and added success styling

--- CPW219-eCommerceSite/Controllers/GamesController.cs
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -1,13 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPW219_eCommerceSite.Data;
+using CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CPW219_eCommerceSite.Controllers
 {
     public class GamesController : Controller
     {
+        private readonly GameContext _context;
+
+        public GamesController(GameContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Game newGame)
+        {
+            if (ModelState.IsValid)
+            {
+                // add to db
+                _context.Games.Add(newGame); // prepares insert
+                _context.SaveChanges(); // executes pending insert
+
+                // show success message on page
+                ViewData["Message"] = $"{newGame.Title} was added successfully!";
+                return View();
+            }
+            return View(newGame);
         }
     }
 }
