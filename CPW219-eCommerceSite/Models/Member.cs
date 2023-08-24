1d4c81dd5700a790f92a52f0b55d2f42e5ffa31e
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-08-24 13:44:19 -0700

    Added a view model for the login page

--- CPW219-eCommerceSite/Models/Member.cs
+++ CPW219-eCommerceSite/Models/Member.cs
@@ -39,4 +39,14 @@ namespace CPW219_eCommerceSite.Models
         [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; } = null!;
     }
+
+    public class LoginViewModel
+    {
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
 }
