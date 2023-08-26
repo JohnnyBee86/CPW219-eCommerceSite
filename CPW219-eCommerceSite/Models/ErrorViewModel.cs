a930222ffed9600b6a835427bb8a91cb0b469661
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-08-26 12:05:21 -0700

    added missing definitions to ErrorViewModel

--- CPW219-eCommerceSite/Models/ErrorViewModel.cs
+++ CPW219-eCommerceSite/Models/ErrorViewModel.cs
@@ -5,8 +5,14 @@ namespace CPW219_eCommerceSite.Models
     /// </summary>
     public class ErrorViewModel
     {
+        /// <summary>
+        /// The requested Id
+        /// </summary>
         public string? RequestId { get; set; }
 
+        /// <summary>
+        /// The error message to be displayed
+        /// </summary>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
