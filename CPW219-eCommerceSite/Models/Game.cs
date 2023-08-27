a3bbc5cac5a82f84a5a7a19f03539fe5d9386688
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-08-27 12:24:45 -0700

    Added CartGameViewModel to handle shopping cart

--- CPW219-eCommerceSite/Models/Game.cs
+++ CPW219-eCommerceSite/Models/Game.cs
@@ -31,4 +31,26 @@ namespace CPW219_eCommerceSite.Models
         [Range(1, 10)]
         public int PlayerCount { get; set; }
     }
+
+    /// <summary>
+    /// A single game that has been added to a shopping cart cookie
+    /// </summary>
+    public class CartGameViewModel
+    {
+        /// <summary>
+        /// The unique identifier of the game in the cart
+        /// </summary>
+        public int GameId { get; set; }
+
+        /// <summary>
+        /// The name of the game in the cart
+        /// </summary>
+        public string Title { get; set; }
+
+		/// <summary>
+		/// How many players will be in the game
+		/// </summary>
+		[Range(1, 10)]
+		public int PlayerCount { get; set; }
+	}
 }
